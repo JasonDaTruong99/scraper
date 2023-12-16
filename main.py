0d8b454ac36fd70828cf91ba291bd5a3a383ee4d
Ryan <rmdupee@hotmail.com>
2023-12-16 17:50:37 -0500

    added basic gpt code for scraper and toyota url

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+
+def scrape_website():
+    url = "https://www.toyota.com/search-inventory/"
+    # Send an HTTP request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extract data using BeautifulSoup methods
+        # Example: Print all the text content of <p> tags
+        for paragraph in soup.find_all('p'):
+            print(paragraph.get_text())
+
+    else:
+        print(f"Failed to retrieve the page. Status code: {response.status_code}")
+
+if __name__ == "__main__":
+    # Replace 'https://example.com' with the URL of the website you want to scrape
+    website_url = 'https://example.com'
+    scrape_website(website_url)
--- readme.txt
+++
@@ -1,1 +0,0 @@
-cumfartcumfartcumfart
