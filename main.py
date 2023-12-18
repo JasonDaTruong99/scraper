4371a3415daf67c23cf81c38a4eebd7058714c11
JasonDaTruong <jasontruong@usf.edu>
2023-12-17 23:18:05 -0500

    rewrote main.py to scrape required data from toyota website and fill it into a created json file toyota.json

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
+import json
 
-    # Define type of session being used for requests lib
+# Define type of session being used for requests lib
 session = HTMLSession()
 
-    # Send an HTTP request to the URL
+# Send an HTTP request to the URL
 response = session.get('https://www.toyota.com/all-vehicles/')
 
 response.html.render()
 
-#print(response.text)
-
+# init variables
 msrp = response.html.find('.header' '.body-01', containing="$")
-for i in range(50):
-    if msrp:
-        print(msrp[i].text)
-    else:
-        print("No MSRP")
-
 model = response.html.find('.title' '.heading-04')
-for i in range(50):
-    if model:
-        print(model[i].text)
-    else:
-        print("No Model")
-
 modelYear = response.html.find('.model-year')
-for i in range(50):
-    if modelYear:
-        print(modelYear[i].text)
-    else:
-        print("No Model Year")
+
+# create a dynamic list to write car data to
+carDataList = []
+
+def getCarData():
+    for i in range(50):
+        # msrp stores dollar amount in reverse correlation to the correct model & year, making an index for it and making it a func of i allows us to get around this
+        msrpIndex = 49 - i
+        carData = {
+            'Model': model[i].text,
+            'Year' : modelYear[i].text,
+            'MSRP' : msrp[msrpIndex].text
+        } 
+        carDataList.append(carData) # append scraped data to list
+
+getCarData() #Call function to fill the list
+
+# Write the list contents into the json file
+filename = "toyota.json"
+with open(filename, "w") as file_object:
+    json.dump(carDataList, file_object, indent = 2)
+
