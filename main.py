26d1b05aedd06100ab538b3a7fc5a30598766664
JasonDaTruong <jasontruong@usf.edu>
2023-12-18 15:13:12 -0500

    edited var names to be more consistent with future updates

--- main.py
+++ main.py
@@ -13,25 +13,26 @@ response.html.render()
 msrp = response.html.find('.header' '.body-01', containing="$")
 model = response.html.find('.title' '.heading-04')
 modelYear = response.html.find('.model-year')
+toyotaQty = 49
 
 # create a dynamic list to write car data to
-carDataList = []
+toyotaDataList = []
 
 def getCarData():
-    for i in range(50):
+    for i in range(toyotaQty):
         # msrp stores dollar amount in reverse correlation to the correct model & year, making an index for it and making it a func of i allows us to get around this
-        msrpIndex = 49 - i
+        msrpIndex = toyotaQty - i
         carData = {
             'Model': model[i].text,
             'Year' : modelYear[i].text,
             'MSRP' : msrp[msrpIndex].text
         } 
-        carDataList.append(carData) # append scraped data to list
+        toyotaDataList.append(carData) # append scraped data to list
 
 getCarData() #Call function to fill the list
 
 # Write the list contents into the json file
 filename = "toyota.json"
 with open(filename, "w") as file_object:
-    json.dump(carDataList, file_object, indent = 2)
+    json.dump(toyotaDataList, file_object, indent = 2)
 
