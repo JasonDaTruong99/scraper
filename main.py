135d9cd8108170b10a1231ff5c843263e9b6688a
Ryan <rmdupee@hotmail.com>
2023-12-19 16:32:12 -0500

    pookie bear
    Got the async scraper stuff working :)

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from requests_html import HTMLSession
+from requests_html import AsyncHTMLSession, HTML
 import json
 
-def getCarData(session, url, qty):
+'''async def getToyotaData(session, qty):
     # Send an HTTP request to the URL
-    response = session.get('https://www.toyota.com/all-vehicles/')
-    response.html.render()
+    response = await get_toyota(session)
 
     carDataList = []
 
@@ -12,12 +11,14 @@ def getCarData(session, url, qty):
     msrp = response.html.find('.header' '.body-01', containing="$")
     model = response.html.find('.title' '.heading-04')
     modelYear = response.html.find('.model-year')
-    
+
+    #Check to see if amount of data matches what's expected and create a list if so, if not print for troubleshooting
     if len(msrp) == len(model) == len(modelYear) == qty:
         for i in range(qty):
             # msrp stores dollar amount in reverse correlation to the correct model & year, making an index for it and making it a func of i allows us to get around this
             msrpIndex = qty - i - 1
             carData = {
+            'Make' : "Toyota",
             'Model': model[i].text,
             'Year' : modelYear[i].text,
             'MSRP' : msrp[msrpIndex].text
@@ -30,18 +31,27 @@ def getCarData(session, url, qty):
         print(len(msrp))
         print(len(model))
         print(len(modelYear))
+        print(qty)
+        return False'''
 
 def writeToJson(dataList, filename):
     with open(filename, "w") as file_object:
         json.dump(dataList, file_object, indent = 2)
 
-def main():
-    # Define type of session being used for requests lib
-    session = HTMLSession()
+'''async def get_toyota(session):
+    try:
+        response = await session.get('https://www.toyota.com/all-vehicles/')
+        await response.html.arender()
+        return response
+    except Exception as e:
+        print(f"Error fetching Toyota data: {e}")
+        return None'''
 
-    url = 'https://www.toyota.com/all-vehicles/'
+async def main():
+    session = AsyncHTMLSession()
+    #ToyotaQty is 50
     toyotaQty = 50
-    toyotaDataList = getCarData(session, url, toyotaQty)
+    toyotaDataList = await getToyotaData(session, toyotaQty)
 
     if toyotaDataList:
         # Call function to write the list contents into the json file
@@ -49,5 +59,8 @@ def main():
         writeToJson(toyotaDataList, filename)
         print(f"Data written to {filename}")
 
+    await session.close()
+
 if __name__ == "__main__":
-    main()
+    import asyncio
+    asyncio.run(main())
